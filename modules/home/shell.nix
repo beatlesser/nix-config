655b362c50ac54908ca6a9209ca69a70083ed202
cyer <uuuuceny@gmail.com>
2025-10-11 15:18:56 +0800

    update

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -5,14 +5,73 @@ in
 {
   programs = {
 
+    #shell
     fish = {
       enable = true;
     };
 
-    git = {
+    #documentation
+    tealdeer = {
       enable = true;
+      enableAutoUpdates = true;
+      settings = {
+        display = {
+          compact = false;
+          use_pager = true;
+        };
+        updates = {
+          auto_update = false;
+          auto_update_interval_hours = 720;
+        };
+      };
+    };
+
+    #git gui wrtitten by go
+    lazygit.enable = true;
+
+    #git config
+    git = {
+
+      enable = true;
+
+      lfs.enable = true;
+
       userName = "${username}";
       userEmail = "${email}";
+
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = true;
+        push.autoSetupRemote = true;
+
+        url = {
+          "ssh://git@github.com/beatlesser" = {
+            insteadOf = "https://github.com/beatlesser";
+          };
+        };
+      };
+
+      # A syntax-highlighting pager for git, diff, grep, and blame output
+      delta = {
+        enable = true;
+        options = {
+          diff-so-fancy = true;
+          line-numbers = true;
+          true-color = "always";
+          # features => named groups of settings, used to keep related settings organized
+          # features = "";
+        };
+      };
+      aliases = {
+        br = "branch";
+        sw = "switch";
+        rs = "reset";
+        rt = "restore";
+        st = "status";
+        cm = "commit -m";
+        ca = "commit -am";
+        caa = "commit --amend -a --no-edit";
+      };
     };
 
     fastfetch.enable = true;
