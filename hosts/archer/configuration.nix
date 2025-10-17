793ce7ba3880891bae8bad426b43f9e54f2921c6
beatlesser <2429209973@qq.com>
2025-10-17 16:46:19 +0800

    update

--- hosts/archer/configuration.nix
+++ hosts/archer/configuration.nix
@@ -19,8 +19,15 @@
     kernelModules = [ ];
   };
 
+  programs.hyprland.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  documentation = {
+    man.enable = false;
+    info.enable = false;
+    nixos.enable = false;
+  };
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
 }
