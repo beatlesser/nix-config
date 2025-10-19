c0ea8ee9e7c7ec97eda7d0296518f583d0be4d20
cyer <uuuuceny@gmail.com>
2025-10-20 03:15:30 +0800

    update

--- hosts/nixer/home.nix
+++ hosts/nixer/home.nix
@@ -1,10 +1,23 @@
-{ myvars, mylib, ... }:
+{
+  myvars,
+  mylib,
+  config,
+  ...
+}:
 let
   inherit (myvars) username;
 in
 {
   programs.home-manager.enable = true;
   programs.nix-index-database.comma.enable = true;
+
+  userDirs = {
+    enable = true;
+    createDirectories = true;
+    extraConfig = {
+      XDG_SCREENSHOTS_DIR = "${config.xdg.userDirs.pictures}/Screenshots";
+    };
+  };
 
   home = {
     inherit username;
