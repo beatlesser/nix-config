865e1a937ec93e813cd93d1e506622f64f7fbcbb
cyer <uuuuceny@gmail.com>
2025-10-21 16:02:01 +0800

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'dankMaterialShell':
        'github:AvengeMedia/DankMaterialShell/5a32398446136b6b3ac69f4966f56f954538cf05' (2025-10-19)
      → 'github:AvengeMedia/DankMaterialShell/507b516f8906eea83d2f3461298fbf99190cbe77' (2025-10-21)
    • Updated input 'dms-cli':
        'github:AvengeMedia/danklinux/6277da1676009439af04421537bf641754ca80ed' (2025-10-17)
      → 'github:AvengeMedia/danklinux/acc067fa706dfc55ac0877ed5e0d8adbafde18f4' (2025-10-21)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/870883ba11ba1c84f756c0c1f9fa74cdb2a16c1e' (2025-10-18)
      → 'github:nix-community/home-manager/84e1adb0cdd13f5f29886091c7234365e12b1e7f' (2025-10-21)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/544961dfcce86422ba200ed9a0b00dd4b1486ec5' (2025-10-15)
      → 'github:nixos/nixpkgs/5e2a59a5b1a82f89f2c7e598302a9cacebb72a67' (2025-10-19)
    • Updated input 'nur':
        'github:nix-community/NUR/fa295f1691beae2d0d3667de5c4c159c56ebfb0c' (2025-10-19)
      → 'github:nix-community/NUR/856cb321307a1f9da698eafbc701b92de54000b3' (2025-10-21)
    • Updated input 'sops-nix':
        'github:Mic92/sops-nix/9c9a9798be331ed3f4b2902933d7677d0659ee61' (2025-10-19)
      → 'github:Mic92/sops-nix/5a7d18b5c55642df5c432aadb757140edfeb70b3' (2025-10-20)

--- pkgs/bilibili/default.nix
+++ pkgs/bilibili/default.nix
@@ -6,34 +6,33 @@
   makeWrapper,
   ...
 }@args:
-  stdenv.mkDerivation rec {
-    pname = "bilibili";
-    version = "1.2.1-1";
+stdenv.mkDerivation rec {
+  pname = "bilibili";
+  version = "1.2.1-1";
 
-    src = fetchurl {
-       url = "https://github.com/msojocs/bilibili-linux/releases/download/v1.2.1-1/io.github.msojocs.bilibili_1.2.1-1_amd64.deb";
+  src = fetchurl {
+    url = "https://github.com/msojocs/bilibili-linux/releases/download/v1.2.1-1/io.github.msojocs.bilibili_1.2.1-1_amd64.deb";
     sha256 = "sha256-t/igezm0ipkOkKION8qTYGK9f6qI3c4iPuS/wWrMywQ=";
-    };
+  };
 
-    unpackPhase = ''
-      ar x ${src}
-      tar xf data.tar.xz
-    '';
+  unpackPhase = ''
+    ar x ${src}
+    tar xf data.tar.xz
+  '';
 
-    buildInputs = [ makeWrapper ];
+  buildInputs = [ makeWrapper ];
 
-    installPhase = ''
-      mkdir -p $out/bin
+  installPhase = ''
+    mkdir -p $out/bin
 
-      cp -r usr/share $out/share
+    cp -r usr/share $out/share
 
-      sed -i "s|Exec=.*|Exec=$out/bin/bilibili|" $out/share/applications/*.desktop
+    sed -i "s|Exec=.*|Exec=$out/bin/bilibili|" $out/share/applications/*.desktop
 
-      cp -r opt/apps/io.github.msojocs.bilibili/files/bin/app $out/opt
+    cp -r opt/apps/io.github.msojocs.bilibili/files/bin/app $out/opt
 
-      makeWrapper ${electron}/bin/electron $out/bin/bilibili \
-       --argv0 "bilibili" \
-       --add-flags "$out/opt/app.asar"
-     '';
+    makeWrapper ${electron}/bin/electron $out/bin/bilibili \
+     --argv0 "bilibili" \
+     --add-flags "$out/opt/app.asar"
+  '';
 }
-
